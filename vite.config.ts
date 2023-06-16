321301a39e0ce40356dde88d248dc893b6a4654f
ylliucoder <ylliucoder@outlook.com>
2023-06-16 11:25:53 +0800

    feat: 添加mockjs接口测试

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import path from "node:path";
 import UnoCss from "unocss/vite";
 import { convertEnv } from "./vite/env";
+import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
 // export default defineConfig({
@@ -15,6 +16,8 @@ import { convertEnv } from "./vite/env";
 // });
 
 export default defineConfig(({ command, mode }) => {
+  console.log(mode);
+
   const viteEnv = convertEnv(loadEnv(mode, process.cwd()));
   return {
     resolve: {
@@ -22,6 +25,13 @@ export default defineConfig(({ command, mode }) => {
         "~": path.resolve(__dirname, "src"),
       },
     },
-    plugins: [vue(), UnoCss()],
+    plugins: [
+      vue(),
+      UnoCss(),
+      viteMockServe({
+        mockPath: "mock",
+        enable: true,
+      }),
+    ],
   };
 });
